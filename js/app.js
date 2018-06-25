09adb5ede095925a9221426d48ca7fdffa5333aa
João-Fonseca <joaomgfonseca@gmail.com>
2018-06-25 13:27:53 +0100

    Memory Game v0.250618
      HTML
      - Arrange row for star rating, move counter, time counter
      JavaScript
      - Add comments to missing sections
      - Introduce startTimer functionality
      - Introduce moveCounter functionality

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 // Create a list that holds all of your cards
 let card  = document.getElementsByClassName("card");
 let cards = [...card];
+
+// variables used in openedCard function
 let openedCards = [];
+
+// var used in enable function
 let matchedCard = document.getElementsByClassName("match");
 
 // Display the cards on the page
 const deck = document.querySelector(".deck");
 
+// variables used in startTimer function
+let second = 0, minute = 0;
+const timer = document.querySelector(".timer");
+
+// variables used in moveCounter
+let moves = 0;
+let counter = document.querySelector(".moves");
+
 document.body.onload = startGame();
 
+// @description startGame function
 function startGame(){
 //   - shuffle the list of cards using the provided "shuffle" method below
   var shuffleCards = shuffle(cards);
@@ -37,22 +50,26 @@ function shuffle(array) {
   return array;
 }
 
+// @description event listener on all cards
 // Set up the event listener for a card. If a card is clicked:
 for (var i = 0; i < cards.length; i++) {
   cards[i].addEventListener('click', displayCard);
   cards[i].addEventListener('click', openedCard);
 }
 
+// @description displayCard function - on click toggle classes
 function displayCard() {
   this.classList.toggle("open");
   this.classList.toggle("show");
   this.classList.toggle("disabled");
 }
 
+// @description openedCard function
 // - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
 function openedCard() {
   openedCards.push(this);
   let length = openedCards.length;
+  moveCounter();
   if (length === 2) {
     if (openedCards[0].innerHTML === openedCards[1].innerHTML) {
       matched();
@@ -62,6 +79,7 @@ function openedCard() {
   }
 }
 
+// @description matched function
 //   - if the list already has another card, check to see if the two cards match
 function matched() {
 //     + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
@@ -71,6 +89,8 @@ function matched() {
   openedCards[1].classList.remove("show", "open");
   openedCards = [];
 }
+
+// @description unmatched function
 //     + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
 function unmatched() {
   disable();
@@ -82,12 +102,14 @@ function unmatched() {
   },400);
 }
 
+// @description disable cards temporarily
 function disable() {
   Array.prototype.filter.call(cards, function(card) {
     card.classList.add("disabled");
   });
 }
 
+// @description disable matched cards and enable cards
 function enable() {
   Array.prototype.filter.call(cards, function(card){
     card.classList.remove("disabled");
@@ -97,5 +119,29 @@ function enable() {
   });
 }
 
+// @description game timer
+function startTimer() {
+  let interval = setInterval(function(){
+    timer.innerHTML = minute + "mins " + second + "secs";
+    second++;
+    if (second === 60) {
+      minute++;
+      second = 0;
+    }
+  },1000);
+}
+
+// @description move counter
 //     + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+function moveCounter() {
+  // increment moves and change innerHTML
+  moves++;
+  counter.innerHTML = moves + " Moves";
+  // startTimer on  first click
+  if (moves === 1) {
+    counter.innerHTML = moves + " Move";
+    startTimer();
+  }
+}
+
 //     + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
